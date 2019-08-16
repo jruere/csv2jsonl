5e197a2bbd50d20be20ca84fd542faa3cabcdac4
Javier Ruere <javier@ruere.com.ar>
2019-08-16 11:38:18 -0300

    Fixes classifier for license, updates supported Python versions.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='csv2jsonl',
-    version='0.1.0',
+    version='0.1.1',
     description='Converts a CSV file to JSONLines.',
     long_description=open('README.rst', 'rt', encoding='utf-8').read(),
     url='https://github.com/jruere/csv2jsonl',
@@ -11,11 +11,11 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
